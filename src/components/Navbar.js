f6a293bf280aa9966a24e5850d0b8a1d85a748b6
mdlumansarkar <122555404+mdlumansarkar@users.noreply.github.com>
2024-10-30 06:03:26 +0600

    aa
    aaa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ let name  = "TextUtils";
     <>
     <nav className={`navbar navbar-expand-lg navbar-${props.mode}  bg-${props.mode}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="/"> {props.title}</a>
+    <a className="navbar-brand" href="/"><p>Md Luman Sarkar</p> <p>helleo</p> {props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
