466d0cf080e421b2358b269e1ba7a0a06da2381a
mdlumansarkar <122555404+mdlumansarkar@users.noreply.github.com>
2024-10-30 23:13:29 +0600

    Update App.js
    s

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ export default function App() {
         {/* // </Router> */}
         <About mode = {mood}/>
         <TextForms showAlert = {showAlert} mode = {mood} toggleMood = {toggleMood} heading = "Enter Your Text Here : Word Counter"/>
-        <p>copyright@ Md Luman Sarkar || 2024</p>
+        <p>                               copyright@ Md Luman Sarkar || 2024</p>
 
     </>
   );
