be85df293c40aa862ed62082b94286c3e6743592
mdlumansarkar <122555404+mdlumansarkar@users.noreply.github.com>
2024-10-30 19:17:44 +0600

    dd
    ddd

--- src/App.js
+++ src/App.js
@@ -65,6 +65,8 @@ export default function App() {
         {/* // </Router> */}
         <About mode = {mood}/>
         <TextForms showAlert = {showAlert} mode = {mood} toggleMood = {toggleMood} heading = "Enter Your Text Here :"/>
+        <p>copyright@ Md Luman Sarkar || 2024</p>
+
     </>
   );
 }
